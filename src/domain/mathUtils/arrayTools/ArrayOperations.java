c7e107cace2ef0e8677b1257b5eb1cb4e213859d
Polo <Polo@Polo-VAIO>
2016-08-01 15:22:48 -0500

    First project definition

--- src/domain/mathUtils/arrayTools/ArrayOperations.java
+++ src/domain/mathUtils/arrayTools/ArrayOperations.java
@@ -17,7 +17,9 @@ public abstract class ArrayOperations {
      * Find the maximum number in a 1D array
      * Supports arrays having  NaN values
      * @param x 1D array of doubles
-     * @return maximum value in the array
+     * @return maximum value in the array.
+     * <p>
+     * NaN if the input array is empty.
      * @author Leopoldo Cendejas-Zaragoza
      */
     public static double findMaximum(double x[]){
@@ -25,11 +27,101 @@ public abstract class ArrayOperations {
             double maximum=x[0];
             for(double number: x){
                 //test if the maximum number is NaN if it is NaN use the minimum value for a double as defined by IEEE for comparison 
-                double temp=Double.isNaN(maximum)? -Double.MAX_VALUE:maximum;  
+                double temp=Double.isNaN(maximum)? -Double.MAX_VALUE:maximum;
+                if(maximum==Double.POSITIVE_INFINITY)
+                    return maximum;
                 if (number>temp)
                     maximum=number;
             }
             return maximum;
+        }
+        else{
+            System.out.println("Input array is empty");
+            return Double.NaN;
+        }
+    }
+   
+    /**
+     * Find the maximum number in a 2D array
+     * Supports arrays having  NaN values
+     * @param x 2D array of doubles
+     * @return maximum value in the array.
+     * <p>
+     * NaN if the input array is empty.
+     * @author Leopoldo Cendejas-Zaragoza
+     */
+    public static double findMaximum(double x[][]){
+            if (x.length!=0){
+            double maximum=x[0][0];
+            for(double[] column: x){
+                for(double number: column){
+                    //test if the maximum number is NaN if it is NaN use the minimum value for a double as defined by IEEE for comparison 
+                    double temp=Double.isNaN(maximum)? -Double.MAX_VALUE:maximum;
+                    if(maximum==Double.POSITIVE_INFINITY)
+                        return maximum;
+                    if (number>temp)
+                        maximum=number;
+                }
+            }
+            return maximum;
+        }
+        else{
+            System.out.println("Input array is empty");
+            return Double.NaN;
+        }
+    }
+  
+    /**
+     * Find the minimum number in a 1D array
+     * Supports arrays having  NaN values
+     * @param x 1D array of doubles
+     * @return minimum value in the array.
+     * <p>
+     * NaN if the input array is empty.
+     * @author Leopoldo Cendejas-Zaragoza
+     */
+    public static double findMinimum(double x[]){
+        if (x.length!=0){
+            double minimum=x[0];
+            for(double number: x){
+                //test if the maximum number is NaN if it is NaN use the maximum value for a double as defined by IEEE for comparison 
+                double temp=Double.isNaN(minimum)? Double.MAX_VALUE:minimum;
+                if(minimum==Double.NEGATIVE_INFINITY)
+                    return minimum;
+                if (number<temp)
+                    minimum=number;
+            }
+            return minimum;
+        }
+        else{
+            System.out.println("Input array is empty");
+            return Double.NaN;
+        }
+    }
+    
+    /**
+     * Find the minimum number in a 2D array
+     * Supports arrays having  NaN values
+     * @param x 2D array of doubles
+     * @return minimum value in the array.
+     * <p>
+     * NaN if the input array is empty.
+     * @author Leopoldo Cendejas-Zaragoza
+     */
+    public static double findMinimum(double x[][]){
+            if (x.length!=0){
+            double minimum=x[0][0];
+            for(double[] column: x){
+                for(double number: column){
+                    //test if the maximum number is NaN if it is NaN use the minimum value for a double as defined by IEEE for comparison 
+                    double temp=Double.isNaN(minimum)? Double.MAX_VALUE:minimum;
+                    if(minimum==Double.NEGATIVE_INFINITY)
+                        return minimum;
+                    if (number<temp)
+                        minimum=number;
+                }
+            }
+            return minimum;
         }
         else{
             System.out.println("Input array is empty");
--- test/domain/mathUtils/arrayTools/ArrayOperationsTest.java
+++ test/domain/mathUtils/arrayTools/ArrayOperationsTest.java
@@ -64,5 +64,139 @@ public class ArrayOperationsTest {
         result=ArrayOperations.findMaximum(x);
         assertEquals(expResult, result, 0);
     }
+
+    /**
+     * Test of findMaximum method, of class ArrayOperations.
+     */
+    @Test
+    public void testFindMaximum_2DArr() {
+        //Test if the method is actually giving a maximum
+        System.out.println("findMaximum 2D");
+        double[][] x = {{10, 40, 3}, {4, 5, -54}, {7, 8, 9}};
+        double expResult = 40;
+        double result = ArrayOperations.findMaximum(x);
+        assertEquals(expResult, result, 0.0);
+        
+        //method should return the maximum value even if there are NaN in the array
+        x=new double[][] {{Double.NaN, 2, 100}, {4, Double.NaN, -54}, {7, 8, 9,102}} ;
+        expResult=102;
+        result=ArrayOperations.findMaximum(x);
+        assertEquals(expResult, result, 0);
+        
+        //method should return positive infinity if the array contains positive infinity
+        x=new double[][] {{Double.NaN, 2, 100}, {4, Double.NaN, -54}, 
+            {Double.POSITIVE_INFINITY, 8, 9,102}} ;
+        expResult=Double.POSITIVE_INFINITY;
+        result=ArrayOperations.findMaximum(x);
+        assertEquals(expResult, result, 0);   
+        
+        //method should return the expected value even if a negative Infinity is present in the array
+        x=new double[][] {{Double.NaN, 2, Double.NEGATIVE_INFINITY}, {4, Double.NaN, -54}, {7, 8, 9,102}} ;
+        expResult=102;
+        result=ArrayOperations.findMaximum(x);
+        assertEquals(expResult, result, 0);
+        
+        //method should return NaN if the input array is empty
+        x=new double[0][0];
+        expResult=Double.NaN;
+        result=ArrayOperations.findMaximum(x);
+        assertEquals(expResult, result,0);
+        
+        //method should return the maximum value even if one dimension of the array is not initialized
+        x=new double[][]{{Double.NaN, 2, 100}};
+        expResult=100;
+        result=ArrayOperations.findMaximum(x);
+        assertEquals(expResult, result, 0);
+        
+        x=new double[][]{{Double.NaN}, {2}, {100}};
+        expResult=100;
+        result=ArrayOperations.findMaximum(x);
+        assertEquals(expResult, result, 0);
+    }
     
+    /**
+     * Test of findMinimum method, of class ArrayOperations.
+     */
+    @Test
+    public void testFindMinimum() {
+        //test if the the method is actually finding the minimum
+        System.out.println("findMinimum");
+        double[] x = {-5,4,3,2,1,4,5,100,2};
+        double expResult = -5;
+        double result = ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);
+        
+        //test what happens if the method is called with an empty input array
+        x=new double[0];
+        expResult=Double.NaN;
+        result= ArrayOperations.findMinimum(x);
+        assertEquals(result,expResult,0);
+        
+        //method should return the minimum value even if there are NaNs in the array
+        x=new double[] {Double.NaN, 2, 100, 0};
+        expResult=0;
+        result=ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);
+        
+        //method should return the minimum number if the array contains positive infinity
+        x=new double[] {3,Double.POSITIVE_INFINITY,-2,Double.NaN,5};
+        expResult=-2;
+        result=ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);
+        
+        //method should negative infinity if the array contains a negative infinity value
+        x=new double[] {3,Double.NEGATIVE_INFINITY,2,Double.NaN,5, Double.POSITIVE_INFINITY};
+        expResult=Double.NEGATIVE_INFINITY;
+        result=ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);
+    }    
+    /**
+     * Test of findMinimum method, of class ArrayOperations.
+     */
+    @Test
+    public void testFindMinimum_2DArr() {
+        //Test if the method is actually giving a minimum
+        System.out.println("findMinimum 2D");
+        double[][] x = {{10, 40, 3}, {4, 5, -54}, {7, 8, 9}};
+        double expResult = -54;
+        double result = ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0.0);
+        
+        //method should return the minimum value even if there are NaNs in the array
+        x=new double[][] {{Double.NaN, 2, 100}, {4, Double.NaN, -54}, {7, 8, 9,102}} ;
+        expResult=-54;
+        result=ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);
+        
+        //method should return the expected value if the array contains positive infinity
+        x=new double[][] {{Double.NaN, 2, 100}, {4, Double.NaN, -54}, 
+            {Double.POSITIVE_INFINITY, 8, 9,102}} ;
+        expResult=-54;
+        result=ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);   
+        
+        //method should return negative infinity if a negative Infinity is present in the array
+        x=new double[][] {{Double.NaN, 2, Double.NEGATIVE_INFINITY}, {4, Double.NaN, -54}, {7, 8, 9,102}} ;
+        expResult=Double.NEGATIVE_INFINITY;
+        result=ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);
+        
+        //method should return NaN if the input array is empty
+        x=new double[0][0];
+        expResult=Double.NaN;
+        result=ArrayOperations.findMaximum(x);
+        assertEquals(expResult, result,0);
+        
+        //method should return the minimum value even if one dimension of the array is not initialized
+        x=new double[][]{{Double.NaN, 2, 100}};
+        expResult=2;
+        result=ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);
+        
+        x=new double[][]{{Double.NaN}, {2}, {100}};
+        expResult=2;
+        result=ArrayOperations.findMinimum(x);
+        assertEquals(expResult, result, 0);
+    }
+
 }
