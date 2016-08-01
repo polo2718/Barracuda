a90dfe8c703f7b4e9bcc6c6d6c229965df84986a
Polo <Polo@Polo-VAIO>
2016-08-01 10:47:05 -0500

    First project definition

--- src/domain/mathUtils/arrayTools/ArrayOperations.java
+++ src/domain/mathUtils/arrayTools/ArrayOperations.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package data.mathUtils.arrayTools;
+package domain.mathUtils.arrayTools;
 
 /**
  * <p>
