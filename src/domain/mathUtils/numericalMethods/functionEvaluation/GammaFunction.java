20dfb32660d6683c766c32bcd74c3d1189b45fb5
Polo <>
2016-08-05 13:56:11 -0500

    Added Lanczos series to gamma function

--- src/domain/mathUtils/numericalMethods/functionEvaluation/GammaFunction.java
+++ src/domain/mathUtils/numericalMethods/functionEvaluation/GammaFunction.java
@@ -19,13 +19,13 @@ public final class GammaFunction implements OneVariableFunction{
     private final static double[] coefficients=
     {
         1.000000000190015,
-        76.1800917297146,
+        76.18009172947146,
         -86.50532032941677,
         24.01409824083091,
         -1.231739572450155,
-        .120865097866179e-2,
-        -.395239384953e-5
-    }; //coefficients for the Lanczos formula
+        0.1208650973866179e-2,
+        -0.5395239384953e-5
+    }; //coefficients for the first Lanczos formula
     
     /**
      * Constructor
@@ -33,6 +33,11 @@ public final class GammaFunction implements OneVariableFunction{
     public GammaFunction(){
     }
     
+    /**
+     * Computes the value of the gamma function within 10e-6 precision
+     * @param x
+     * @return 
+     */
     @Override
     public double value(double x){
         //test if the value is a negative integer
@@ -42,20 +47,29 @@ public final class GammaFunction implements OneVariableFunction{
         //if x is between 0 (exclusive) and 1 (inclusive) then use recursion formula gamma(x+1)=x*gamma(x)
         //or gamma(x)=gamma(x+1)/x
         if(x<=1 && x>0){
-            return value((x+1/x));
+            return value(x+1)/x;
         }
         //if x is negative use reflection formula
         if(x<0){
            return Math.PI/(value(1-x)*Math.sin(Math.PI*x));
         }
-        //If x is positive use Lanczo's series approximation
-        if (x>0){
-            return 0;
+        //If x is positive use first Lanczo's series approximation
+        if (x>1){
+            return Math.exp(leadingTerm(x))*series(x)*sqrt2Pi/x;
         }
-        else return Double.NaN;
-        
+        else return Double.NaN;  
+    }
+    
+    /**
+     * Returns the value for the natural logarithm for the game function
+     * @param x
+     * @return ln(gamma(x))
+     */
+    public double logValue(double x){
+       return Math.log(value(x));
     }
    
+    
     /**
      * This function tests if the value x is a Negative Integer
      * @param x - value to test
@@ -66,6 +80,31 @@ public final class GammaFunction implements OneVariableFunction{
             return false;
         //a number is an integer if x%1 is equal to zero
         return x%1==0 && x<0; 
+    }
+    
+    /**
+     * This function returns the series term of the Lanczo's Formula
+     * @param x
+     * @return result of the series term on the Lanczo's formula
+     */
+    private double series(double x){
+        double result=coefficients[0];
+        double temp;
+        for(int i=1; i<=6; i++){
+            temp=coefficients[i]/(x+i);
+            result+=temp;
+        }
+        return result;
+    }
+    
+    /**
+     * This function return the leading term of the Lanczo's Formula
+     * @param x
+     * @return 
+     */
+    private double leadingTerm(double x){
+        double temp=x+5.5;
+        return Math.log(temp)*(x+0.5)-temp;
     }
     
 }
--- test/domain/mathUtils/numericalMethods/functionEvaluation/GammaFunctionTest.java
+++ test/domain/mathUtils/numericalMethods/functionEvaluation/GammaFunctionTest.java
@@ -45,8 +45,76 @@ public class GammaFunctionTest {
     public void testValue() {
         // test that the Gamma function is giving NaN for negative integers
         double expResult=Double.NaN;
-        double result=gamma.value(-243);
+        double result=gamma.value(-243);     
+        System.out.println("gamma(-243)");
+        System.out.println("Expected result="+expResult);     
+        System.out.println("Result="+result);
         assertEquals(expResult, result, 0);
+        System.out.println("Test passed!!\n");
+        
+        //test the returned value for the Gamma function gamma(4)=6
+        expResult=6;
+        result=gamma.value(4);
+        System.out.println("gamma(4)");
+        System.out.println("Expected result="+expResult);     
+        System.out.println("Result="+result);
+        assertEquals(expResult, result, 1e-10);
+        System.out.println("Test passed!!\n");
+        
+        //test a positive value which is less than 1 and greater than 0 gamma(0.55)
+        expResult=1.61612426873;
+        result=gamma.value(0.55);
+        System.out.println("gamma(0.55)");
+        System.out.println("Expected result="+expResult);     
+        System.out.println("Result="+result);
+        assertEquals(expResult, result, 1e-10);
+        System.out.println("Test passed!!\n");
+        
+        //test a non integer negative value gamma(-7.5)
+        expResult=2.2384932886E-4;
+        result=gamma.value(-7.5);
+        System.out.println("gamma(-7.5)");
+        System.out.println("Expected result="+expResult);     
+        System.out.println("Result="+result);
+        assertEquals(expResult, result, 1e-10);
+        System.out.println("Test passed!!\n");
+        
+        //test another positive value for the gama function gamma(5.4)
+        expResult=44.5988481451;
+        result=gamma.value(5.4);
+        System.out.println("gamma(5.4)");
+        System.out.println("Expected result="+expResult);     
+        System.out.println("Result="+result);
+        assertEquals(expResult, result, 1e-10);
+        System.out.println("Test passed!!\n");
+        
+        //test another positive value for the gama function gamma(10)
+        expResult=362880;
+        result=gamma.value(10);
+        System.out.println("gamma 10");
+        System.out.println("Expected result="+expResult);     
+        System.out.println("Result="+result);
+        assertEquals(expResult, result, 1e-6);
+        System.out.println("Test passed!!");
+        
+        //test positive value>12 for the gama function gamma(12)
+        expResult=39916800;
+        result=gamma.value(12.1);
+        System.out.println("gamma 10");
+        System.out.println("Expected result="+expResult);     
+        System.out.println("Result="+result);
+        assertEquals(expResult, result, 1e-6);
+        System.out.println("Test passed!!");
+        
+        
+        //test another positive value for the gama function gamma(3/2)
+        expResult=Math.sqrt(Math.PI/2);
+        result=gamma.value(0.5);
+        System.out.println("gamma(3/2)");
+        System.out.println("Expected result="+expResult);     
+        System.out.println("Result="+result);
+        assertEquals(expResult, result, 1e-10);
+        System.out.println("Test passed!!");
         
     }
     
